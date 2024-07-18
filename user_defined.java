7a398da91d4fd8d0c1ff54b2eace2f2ebce69126
avishkar0950 <163967742+avishkar0950@users.noreply.github.com>
2024-07-18 17:54:48 +0530

    Add files via upload

---
+++ user_defined.java
@@ -0,0 +1,16 @@
+package practice_programs;
+import java.io.*;
+import java.util.*;
+
+public class user_defined {
+
+	public static void main(String[] args)
+	{
+		int a,b;
+		System.out.println("enter the value of a&b");
+		Scanner std=new Scanner(System.in);
+		a=std.nextInt();
+		b=std.nextInt();
+		System.out.println("addition of 2nos " +(a+b));
+	}
+}
