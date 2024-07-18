20f531914d0bcd7c8118a8d3de9b34d2174bd171
avishkar0950 <163967742+avishkar0950@users.noreply.github.com>
2024-07-18 17:54:00 +0530

    Add files via upload

---
+++ first_program.java
@@ -0,0 +1,16 @@
+package practice_programs;
+
+import java.util.Scanner;
+
+public class first_program {
+
+	public static void main(String[] args)
+	{
+		int a,b;
+		System.out.println("enter two nos");
+		Scanner std=new Scanner(System.in);
+		a=std.nextInt();
+		b=std.nextInt();
+		System.out.println("additon of two nos"+(a+b));
+	}
+}
