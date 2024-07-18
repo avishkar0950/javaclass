43329bd9efb5ad8f9ea65082b11d28dfebb7542f
avishkar0950 <163967742+avishkar0950@users.noreply.github.com>
2024-07-18 17:54:22 +0530

    Add files via upload

---
+++ substraction_user.java
@@ -0,0 +1,17 @@
+
+package practice_programs;
+
+import java.util.Scanner;
+
+public class substraction_user {
+	public static void main(String[] args)
+	{
+		int a,b;
+		System.out.println("enter two nos");
+		Scanner std=new Scanner(System.in);
+		a=std.nextInt();
+		b=std.nextInt();
+		System.out.println("substraction of two nos"+(a-b));
+	}
+
+}
