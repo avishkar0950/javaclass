8a32f705308c354f5d5af3f5ff61976a4ca9b2c5
avishkar0950 <163967742+avishkar0950@users.noreply.github.com>
2024-07-30 10:08:40 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,20 @@
+package avishkarjava;
+import java.util.*;
+public class array {
+
+	
+         public static void main(String[]args)
+         {
+        	 int i;
+        	 int arr[]=new int[5];
+        			 arr[0]=12;
+        	         arr[1]=15;
+        	         arr[2]=17;
+        	         arr[3]=20;
+        	         arr[4]=22;
+        	         for(i=0;i<5;i++)
+        	         {
+        	        	 System.out.println(arr[i]);
+        	         }
+         }
+}
