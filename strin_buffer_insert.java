1c45940595cba59b8a81986193ae76da315fee95
avishkar0950 <163967742+avishkar0950@users.noreply.github.com>
2024-08-03 17:18:46 +0530

    Add files via upload

---
+++ strin_buffer_insert.java
@@ -0,0 +1,14 @@
+package avishkar_programs;
+import java.io.*;
+public class strin_buffer_insert {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	    StringBuffer str=new StringBuffer("shree computer");
+	    System.out.println(str.insert(12,"education"));
+	    System.out.println(str.delete(13,15));
+	    System.out.println(str.charAt(5));
+	    System.out.println(str.delete(2, 5));
+	}
+
+}
