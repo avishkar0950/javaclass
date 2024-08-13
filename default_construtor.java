3a6c955bc7579eb3968395c3b4da72e179fd09f2
avishkar0950 <163967742+avishkar0950@users.noreply.github.com>
2024-08-13 17:49:39 +0530

    Add files via upload

---
+++ default_construtor.java
@@ -0,0 +1,23 @@
+package avishkar_programs;
+import java.io.*;
+import java.util.*;
+class def_const
+{
+	def_const()
+	{
+		int a,b;
+		Scanner std=new Scanner(System.in);
+		a=std.nextInt();
+		b=std.nextInt();
+		System.out.println("additiobn is "+(a+b));
+	}
+}
+public class default_construtor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		def_const d1=new def_const();
+		
+	}
+
+}
