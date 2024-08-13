fc0f242042a6abe617c68840a07cce71149d61eb
avishkar0950 <163967742+avishkar0950@users.noreply.github.com>
2024-08-13 17:49:16 +0530

    Add files via upload

---
+++ arguments.java
@@ -0,0 +1,15 @@
+package avishkar_programs;
+import java.io.*;
+import java.util.*;
+public class arguments {
+public static int add(int a,int b)
+{
+	return a+b;
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=85,b=78;
+		System.out.println(add(a,b));
+	}
+
+}
